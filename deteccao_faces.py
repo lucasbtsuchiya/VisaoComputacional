40c04b4f3977932cc060e1455f75aa180d4df065
lucasbtsuchiya <48805920+lucasbtsuchiya@users.noreply.github.com>
2020-01-31 12:36:19 -0300

    Update deteccao_faces.py

--- deteccao_faces.py
+++ deteccao_faces.py
@@ -6,7 +6,11 @@ imagem = cv2.imread('pessoas.jpg')
 classificador = cv2.CascadeClassifier('haarcascade_frontalface_defult.xml')
 imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-deteccoes = classificador.detectMultiScale(imagemcinza)
+#Parametros:
+#scaleFactor=1.09,
+# minNeighbors 
+
+deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor)
 
 print(deteccoes)
 #Quantas pessoas conseguiu detectar
