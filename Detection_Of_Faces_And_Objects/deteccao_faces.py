df5035a4368e5bf8cffdec431676e51bfd9c8e1b
Lucas Asus <lucasbtsuchiya@gmail.com>
2020-02-02 11:02:56 -0300

    Commit teste

--- Detection_Of_Faces_And_Objects/deteccao_faces.py
+++ Detection_Of_Faces_And_Objects/deteccao_faces.py
@@ -1,12 +1,26 @@
 #Importando biblioteca OpenCV
 import cv2
 
+print(cv2.__version__)
 imagem = cv2.imread('pessoas.jpg')
-classificador = cv2.CascadeClassifier('haarcascade_frontalface_defult.xml')
+
+classificador = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-deteccoes = classificador.detecMultiScale(imagemcinza)
+# Parâmetros do detectMultiScale
+# Scale Factor scaleFactor = valor_da_escala
+# minNeighbors minNeighbors = valor
+# minSize
+deteccoes = classificador.detectMultiScale(imagemcinza, scaleFactor=1.09, minNeighbors=5, minSize=(30,30), maxSize=(40,40))
 
 print(deteccoes)
 print(len(deteccoes))
-##Teste
+
+# Haardcascade (OpenCV)
+# Marcar as faces detectadas
+for (x, y, l, a) in deteccoes:
+    cv2.rectangle(imagem, (x, y), (x + l, y + a), (0,255,0), 2)
+
+cv2.imshow('Detecctor de faces', imagem)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
